e90717d34516869f561ab139a1602dc0c88d567c
mikako-m <chinatsu.m.19.06.19@gmail.com>
2024-09-09 23:15:03 +0900

    kadai.mdn.jsのアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const date = today.getDate();
+
+console.log(year+'年');console.log(month+'月');console.log(date+'日');
+
